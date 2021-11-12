c6c8efb89327e14294230ee3b786a1528e8f7a1b
Kirill Titov <ktitov@aorti.ru>
2021-11-12 16:24:02 +0300

    cleaning packege

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ const errorMiddleware = require("./middleware/error-middleware");
 const logMiddleware = require("./middleware/log-middleware");
 const router = require("./routers/index");
 const {io} = require('./handler/indexHandler')
-// const { initDatabase } = require("./database/index");
 
 const serverPort = process.env.PORT || 3000;
-const serverHost = process.env.HOSTNAME || '0.0.0.0';
+const serverHost = process.env.HOSTNAME || 'localhost';
 const app = express();
 const server = createServer(app);
 
@@ -20,9 +19,6 @@ app.use(express.json());
 app.use(logMiddleware);
 app.use('/api', router);
 app.use(errorMiddleware);
-
-// createSecretCode();
-// initDatabase();
 
 server.listen(serverPort, serverHost, () =>
   console.info(`Server is listening on http://${serverHost}:${serverPort}`)
